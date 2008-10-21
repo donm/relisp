499b8ad68cd42b29a4a704778687b977d7bf7d70
Don March <don@compy>
2008-10-21 07:17:18 -0400

    working versions of ruby and elisp.

--- relisp.el
+++ relisp.el
@@ -1,27 +1,56 @@
 (defconst relisp-terminal-string "__terminal-string__")
 (defconst relisp-terminal-regexp (concat relisp-terminal-string "[[:space:]]*"))
 
-(defun relisp-receiver (closure output)
-  (message (comment-string-strip 
-	    (car (split-string output relisp-terminal-string)) t t)))
-  
-(setq relisp-controller-process 
-      (start-process "relisp-controller" nil "/home/don/src/relisp/relisp_controller"))
-(setq relisp-tq 
-      (tq-create relisp-controller-process))
+(defun trim-leading-whitespace (str)
+  "..."
+  (let ((s (if (symbolp str)(symbol-name str) str))
+	(whitespace-regexp "\\( \\|\f\\|\t\\|\n\\)"))
+    (save-excursion
+      (while (and
+	      (not (null (string-match (concat "^" whitespace-regexp) s)))
+	      (> (length s) (string-match (concat "^" whitespace-regexp) s)))
+	(setq s (replace-match "" t nil s))))
+    s))
 
+(defun trim-trailing-whitespace (str)
+  "..."
+  (let ((s (if (symbolp str)(symbol-name str) str))
+	(whitespace-regexp "\\( \\|\f\\|\t\\|\n\\)"))
+    (save-excursion
+      (while (and
+	      (not (null (string-match (concat whitespace-regexp "$") s)))
+	      (> (length s) (string-match (concat whitespace-regexp "$") s)))
+	(setq s (replace-match "" t nil s))))
+    s))
+
+(defun chomp (str)
+  "..."
+  (trim-leading-whitespace (trim-trailing-whitespace str)))
+
+(defun relisp-receiver (closure output)
+  (message (chomp (car (split-string output relisp-terminal-string))) t t))
+
+(defun relisp-controller-alive-p nil
+  (equal (process-status relisp-controller-process) 'run))
+  
+(defun relisp-start-controller nil
+  (setq relisp-controller-process 
+	(start-process "relisp-controller" nil "/home/don/src/relisp/relisp_controller"))
+  (setq relisp-tq 
+	(tq-create relisp-controller-process)))
 
 (defun ruby-eval (code)
+  (unless (relisp-controller-alive-p)
+    (relisp-start-controller))
   (let ((message (concat relisp-terminal-string "\n" 
 			 code                   "\n" 
 			 relisp-terminal-string "\n")))
-    (if (equal (process-status relisp-controller-process) 'run)
+    (if (relisp-controller-alive-p)
 	(tq-enqueue relisp-tq message relisp-terminal-regexp nil 'relisp-receiver))))
   
-(ruby-eval "a = 3")
-
-(ruby-eval "2 + a")
-
+(ruby-eval "2 + 3")
+(ruby-eval "2 + 9")
+(ruby-eval "2 + 1")
 
 ;; (defun relisp-filter-function (process output)
 ;;   (setq return (eval (read output)))
